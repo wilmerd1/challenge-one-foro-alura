beb2f26acdffd4308efa059c54f0aa6c206a0fd1
Wilmer Andres Duque Olaya <44647699+wilmerd1@users.noreply.github.com>
2023-05-22 21:25:46 -0500

    api

---
+++ src/main/resources/db.migration/V1__create-table-topicos.sql
@@ -0,0 +1,12 @@
+create table topicos(
+
+                        id bigint not null auto_increment,
+                        titulo varchar(100) not null unique,
+                        mensaje varchar(900) not null,
+                        fecha_creacion DATETIME  not null,
+                        estatus_topico varchar(100) not null,
+                        autor varchar(100) not null,
+                        curso varchar(100) not null,
+                        primary key(id)
+
+);
